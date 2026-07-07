4ae087afbb7d8bd758516bc4592f7524a16a46a0
yievern <yievern050524@gmail.com>
2026-07-07 14:00:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-fqwehqi
-shahash
-iadhaiudnai
-hihihi
-haidhqodaa
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blind Bite</title>
+</head>
+<body>
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/navigation.php'); ?>
+        <img src="http://localhost/wadassignment/images/bg.jpg" width="100%">
+        <article>
+            <p>
+        <article>
+            <p>
+			
+            </p>
+        </article>            </p>
+        </article>
+    <?php include('includes/footer.php'); ?>
+    <script src="js/script.js"></script>
+</body>
+</html>
