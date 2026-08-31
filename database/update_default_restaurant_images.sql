c610f4434da84fe6160d988900e0f684468df1c7
leezhenyong090 <leezhenyong090@gmail.com>
2026-08-31 16:08:11 +0800

    update images

--- database/update_default_restaurant_images.sql
+++ database/update_default_restaurant_images.sql
@@ -4,14 +4,28 @@ USE blindbite;
 -- generating each restaurant image URL.
 UPDATE restaurants
 SET blind_box_image = CASE restaurant_name
+   
+    WHEN 'Amitie Cafe' THEN 'Amitie Cafe.jpg'
+    WHEN 'Domino (Sg Long)' THEN 'Domino (Sg Long).webp'
+    WHEN 'K Cafe' THEN 'K Cafe.webp'
+    WHEN 'KFC Bandar Sungai Long' THEN 'KFC Bandar Sungai Long.webp'
     WHEN 'Myosotis Cafe' THEN 'Myosotis Cafe.webp'
-    WHEN 'Sushi Mentai' THEN 'Sushi Mentai.webp'
     WHEN 'QQ Pan Mee & Ramen Restaurant' THEN 'QQ Pan Mee & Ramen Restaurant.jpg'
+    WHEN 'Restaurant Ibrahim Maju Bistro' THEN 'Restaurant Ibrahim Maju Bistro.webp'
+    WHEN 'Secret Recipe (Sg Long)' THEN 'Secret Recipe (Sg Long).webp'
+    WHEN 'Sushi Mentai' THEN 'Sushi Mentai.webp'
     ELSE blind_box_image
 END
 WHERE restaurant_name IN (
+    'Amitie Cafe',
+    'Domino (Sg Long)',
+    'K Cafe',
+    'KFC Bandar Sungai Long',
     'Myosotis Cafe',
-    'Sushi Mentai',
-    'QQ Pan Mee & Ramen Restaurant'
+    'QQ Pan Mee & Ramen Restaurant',
+    'Restaurant Ibrahim Maju Bistro',
+    'Secret Recipe (Sg Long)',
+    'Sushi Mentai'
+
 );
 
