9a8d83fabb57ffd4cff44123d2ac7ffc5008da37
yievern <yievern050524@gmail.com>
2026-08-31 17:40:58 +0800

    picc

--- database/update_default_restaurant_images.sql
+++ database/update_default_restaurant_images.sql
@@ -8,7 +8,7 @@ SET blind_box_image = CASE restaurant_name
     WHEN 'Amitie Cafe' THEN 'Amitie Cafe.jpg'
     WHEN 'Domino Pizza (Sungai Long)' THEN 'domino.jpg'
     WHEN 'K Cafe' THEN 'K Cafe.webp'
-    WHEN 'KFC Bandar Sungai Long' THEN 'KFC Bandar Sungai Long.wbep'
+    WHEN 'KFC Bandar Sungai Long' THEN 'KFC Bandar Sungai Long.jpg'
     WHEN 'Myosotis Cafe' THEN 'Myosotis Cafe.webp'
     WHEN 'QQ Pan Mee & Ramen Restaurant' THEN 'QQ Pan Mee & Ramen Restaurant.jpg'
     WHEN 'Restoran Ibrahim Maju Bistro' THEN 'Restoran Ibrahim Maju Bistro.jpg'
