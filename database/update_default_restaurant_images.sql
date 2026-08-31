441e2e99c700b33a99845290ea8ee45d3eeb4522
leezhenyong090 <leezhenyong090@gmail.com>
2026-08-31 17:01:05 +0800

    update images

--- database/update_default_restaurant_images.sql
+++ database/update_default_restaurant_images.sql
@@ -6,24 +6,24 @@ UPDATE restaurants
 SET blind_box_image = CASE restaurant_name
    
     WHEN 'Amitie Cafe' THEN 'Amitie Cafe.jpg'
-    WHEN 'Domino (Sg Long)' THEN 'Domino (Sg Long).webp'
+    WHEN 'Domino Pizza (Sungai Long)' THEN 'domino.webp'
     WHEN 'K Cafe' THEN 'K Cafe.webp'
-    WHEN 'KFC Bandar Sungai Long' THEN 'KFC Bandar Sungai Long.webp'
+    WHEN 'KFC Bandar Sungai Long' THEN 'KFC Bandar Sungai Long.wbep'
     WHEN 'Myosotis Cafe' THEN 'Myosotis Cafe.webp'
     WHEN 'QQ Pan Mee & Ramen Restaurant' THEN 'QQ Pan Mee & Ramen Restaurant.jpg'
-    WHEN 'Restaurant Ibrahim Maju Bistro' THEN 'Restaurant Ibrahim Maju Bistro.webp'
+    WHEN 'Restoran Ibrahim Maju Bistro' THEN 'Restoran Ibrahim Maju Bistro.jpg'
     WHEN 'Secret Recipe (Sg Long)' THEN 'Secret Recipe (Sg Long).webp'
     WHEN 'Sushi Mentai' THEN 'Sushi Mentai.webp'
     ELSE blind_box_image
 END
 WHERE restaurant_name IN (
     'Amitie Cafe',
-    'Domino (Sg Long)',
+    'Domino Pizza (Sungai Long)',
     'K Cafe',
     'KFC Bandar Sungai Long',
     'Myosotis Cafe',
     'QQ Pan Mee & Ramen Restaurant',
-    'Restaurant Ibrahim Maju Bistro',
+    'Restoran Ibrahim Maju Bistro',
     'Secret Recipe (Sg Long)',
     'Sushi Mentai'
 
